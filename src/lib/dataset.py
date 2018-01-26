793e983ecbe0cde4ae4b286a9cdd1d94caccfe3a
Andriy Taran <andreitdeveloper@gmail.com>
2018-01-26 15:45:49 +0100

    mongo request and response json data

--- src/lib/dataset.py
+++ src/lib/dataset.py
@@ -21,14 +21,30 @@ def dataset(request):
 
 
 if __name__ == '__main__':
-    states = [{"$group": {
+    breast_cancer_by_state = [{"$group": {
         "_id": "$state",
         "count": {"$sum": 1}}},
         {"$sort": SON([("count", -1), ("_id", -1)])}]
 
-    age_35_39_annualy_diagnosed = [
+    breast_cancer_at_a_glance = [
+        {"$group": {
+            "_id": "$year-of-diagnosis",
+            "count": {"$sum": 1}}},
+        {"$sort": SON([("_id", 1)])}]
+
+    breast_cancer_by_age = [
+        {"$group": {
+            "_id": "$age-recode-with-1-year-olds",
+            "count": {"$sum": 1}}},
+        {"$sort": SON([("_id", 1)])}]
+
+    age_30_39_annualy_diagnosed = [
         {"$match": {
-            "age-recode-with-1-year-olds": "35-39 years"}},
+            "$or": [
+                {"age-recode-with-1-year-olds": "35-39 years"},
+                {"age-recode-with-1-year-olds": "30-34 years"}
+            ]
+        }},
         {"$group": {
             "_id": "$year-of-diagnosis",
             "count": {"$sum": 1}}},
@@ -75,7 +91,6 @@ if __name__ == '__main__':
         {"$group": {
             "_id": "$race-recode-w-b-ai-api",
             "count": {"$sum": 1},
-            # "totalDocument": {"$sum": "$race-recode-w-b-ai-api"}
         }},
         {"$project": {
             "count": 1,
@@ -83,7 +98,117 @@ if __name__ == '__main__':
         }},
         {"$sort": SON([("_id", -1)])}]
 
-    request = percent_of_women_with_cancer_by_race
+    surgery_decisions_within_ages_30_40 = [
+        {"$match": {
+            "$or": [
+                {"age-recode-with-1-year-olds": "35-39 years"},
+                {"age-recode-with-1-year-olds": "30-34 years"}
+            ]
+        }},
+        {"$group": {
+            "_id": "$surgery",
+            "count": {"$sum": 1}}},
+        {"$sort": SON([("_id", 1)])}]
+
+    chemotherapy_for_ages_30_40 = [
+        {"$match": {
+            "$or": [
+                {"age-recode-with-1-year-olds": "35-39 years"},
+                {"age-recode-with-1-year-olds": "30-34 years"}
+            ]
+        }},
+        {"$group": {
+            "_id": "",
+            "total": {"$sum": 1},
+            "chemo_set": {"$push": "$chemo"}
+        }},
+        {"$unwind": "$chemo_set"},
+        {"$group": {
+            "_id": {"chemo": "$chemo_set", "total": "$total"},
+            "count": {"$sum": 1}}},
+        {"$project": {
+            "count": 1,
+            "percentage": {"$multiply": [{"$divide": [100, "$_id.total"]}, "$count"], }
+        }},
+        {"$sort": SON([("_id", 1)])}]
+
+    radiation_for_ages_30_40 = [
+        {"$match": {
+            "$or": [
+                {"age-recode-with-1-year-olds": "35-39 years"},
+                {"age-recode-with-1-year-olds": "30-34 years"}
+            ]
+        }},
+        {"$group": {
+            "_id": "",
+            "total": {"$sum": 1},
+            "data_subset": {"$push": "$radiation"}
+        }},
+        {"$unwind": "$data_subset"},
+        {"$group": {
+            "_id": {"radiation": "$data_subset", "total": "$total"},
+            "count": {"$sum": 1}}},
+        {"$project": {
+            "count": 1,
+            "percentage": {"$multiply": [{"$divide": [100, "$_id.total"]}, "$count"], }
+        }},
+        {"$sort": SON([("_id", 1)])}]
+
+    survival_months_within_ages_30_40 = [
+        {"$match": {
+            "$or": [
+                {"age-recode-with-1-year-olds": "35-39 years"},
+                {"age-recode-with-1-year-olds": "30-34 years"}
+            ]
+        }},
+        {"$group": {
+            "_id": "$survival-months",
+            "count": {"$sum": 1}}},
+        {"$sort": SON([("_id", -1)])}]
+
+    cause_of_death_overall = [
+        {"$match": {
+            "cod-to-site-recode": {"$nin": ["Alive"]}}
+        },
+        {"$group": {
+            "_id": "",
+            "total": {"$sum": 1},
+            "data_subset": {"$push": "$cod-to-site-recode"}
+        }},
+        {"$unwind": "$data_subset"},
+        {"$group": {
+            "_id": {"cod-to-site-recode": "$data_subset", "total": "$total"},
+            "count": {"$sum": 1}}},
+        {"$project": {
+            "count": 1,
+            "percentage": {"$multiply": [{"$divide": [100, "$_id.total"]}, "$count"], }
+        }},
+        {"$sort": SON([("count", -1)])}]
+
+    cause_of_death_within_ages_30_40 = [
+        {"$match": {
+            "$and": [{"cod-to-site-recode": {"$nin": ["Alive"]}},
+                     {"$or": [
+                         {"age-recode-with-1-year-olds": "35-39 years"},
+                         {"age-recode-with-1-year-olds": "30-34 years"}
+                     ]}]
+        }},
+        {"$group": {
+            "_id": "",
+            "total": {"$sum": 1},
+            "data_subset": {"$push": "$cod-to-site-recode"}
+        }},
+        {"$unwind": "$data_subset"},
+        {"$group": {
+            "_id": {"cod-to-site-recode": "$data_subset", "total": "$total"},
+            "count": {"$sum": 1}}},
+        {"$project": {
+            "count": 1,
+            "percentage": {"$multiply": [{"$divide": [100, "$_id.total"]}, "$count"], }
+        }},
+        {"$sort": SON([("count", -1)])}]
+
+    request = cause_of_death_overall
 
     json_output = dataset(request)
     pprint(json_output)
