853a431b0c701f8645e25e7db449658cce623baa
Andriy Taran <andreitdeveloper@gmail.com>
2018-01-28 15:35:25 +0800

    server ip

--- src/static/utils/config.js
+++ src/static/utils/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://localhost:8000';
+export const SERVER_URL = 'http://52.14.12.34';
 
 // config should use named export as there can be different exports,
 // just need to export default also because of eslint rules
