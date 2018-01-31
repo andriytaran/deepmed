f0eac8aeaf35de98c8a5ee2ee639692d5eb946ae
Andriy Taran <andreitdeveloper@gmail.com>
2018-01-31 09:37:19 +0100

    Update API endpoint for "Specific Stats" and "Similar Diagnoses" pages

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -6,10 +6,10 @@ class DiagnosisSerializer(serializers.Serializer):
     Serializer for diagnosis request input details.
     """
     age = serializers.IntegerField(required=True)
+    tumor_size_in_mm = serializers.IntegerField(required=True)
     tumor_grade = serializers.IntegerField(required=True)
     er_status = serializers.CharField(required=True)
     pr_status = serializers.CharField(required=True)
-    tumor_size_in_mm = serializers.IntegerField(required=True)
-    num_pos_nodes = serializers.IntegerField(required=True)
     her2_status = serializers.CharField(required=True)
+    num_pos_nodes = serializers.IntegerField(required=True)
     ethnicity = serializers.CharField(required=False)
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,4 @@
-from pprint import pprint
+import json
 
 from django.conf import settings
 from pymongo import MongoClient
@@ -8,53 +8,53 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from base.serializers import DiagnosisSerializer
-from lib.dataset import breast_cancer_at_a_glance, breast_cancer_by_age, \
-    breast_cancer_by_grade
+from dataset import woman_age_30_40_annualy_diagnosed, \
+    er_pos_pr_neg_her2_neg_annual_diagnoses, breast_cancer_by_size_age_30_40, \
+    distribution_of_stage_of_cancer_for_ages_30_40, \
+    percent_of_women_with_cancer_by_race, surgery_decisions_within_ages_30_40, \
+    chemotherapy_for_ages_30_40, radiation_for_ages_30_40, \
+    survival_months_within_ages_30_40, cause_of_death_overall, \
+    cause_of_death_within_ages_30_40
 
-
-class ProtectedDataView(GenericAPIView):
-    serializer_class = DiagnosisSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            diagnosis = serializer.validated_data
-
-            mongo_client = MongoClient(settings.MONGODB_HOST,
-                                       settings.MONGODB_PORT)
-            collection = mongo_client[settings.DBS_NAME][
-                settings.COLLECTION_NAME]
-
-            collection.insert_one(diagnosis)
-
-            return Response(data=serializer.data,
-                            status=status.HTTP_201_CREATED)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+from lib.dataset import breast_cancer_by_grade, diagnosis, surgery_decisions
 
 
 class ReportDataView(GenericAPIView):
     serializer_class = DiagnosisSerializer
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
+    def get(self, request):
 
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
-            diagnosis = serializer.validated_data
-            pprint(diagnosis)
+            diagnosis_data = serializer.validated_data
 
             data = {
-                # 'breast_cancer_by_state': breast_cancer_by_state(),
-                'breast_cancer_at_a_glance': breast_cancer_at_a_glance(),
-                'breast_cancer_by_age': breast_cancer_by_age(),
-                'breast_cancer_by_grade': breast_cancer_by_grade(
-                    diagnosis['age'])
+                'woman_age_30_40_annualy_diagnosed': woman_age_30_40_annualy_diagnosed(),
+                'er_pos_pr_neg_her2_neg_annual_diagnoses': er_pos_pr_neg_her2_neg_annual_diagnoses(),
+                'breast_cancer_by_grade_and_size_age_30_40': {
+                    'grade': breast_cancer_by_grade(diagnosis_data.get('age')),
+                    'size': breast_cancer_by_size_age_30_40()
+                },
+                'distribution_of_stage_of_cancer_for_ages_30_40': distribution_of_stage_of_cancer_for_ages_30_40(),
+                'percent_of_women_with_cancer_by_race': percent_of_women_with_cancer_by_race(),
+                'surgery_decisions_within_ages_30_40': surgery_decisions_within_ages_30_40(),
+                'chemotherapy_for_ages_30_40': chemotherapy_for_ages_30_40(),
+                'radiation_for_ages_30_40': radiation_for_ages_30_40(),
+                'survival_months_within_ages_30_40': survival_months_within_ages_30_40(),
+                'cause_of_death': {
+                    'cause_of_death_overall': cause_of_death_overall(),
+                    'cause_of_death_within_ages_30_40': cause_of_death_within_ages_30_40()
+                },
+                'surgery_decisions': surgery_decisions(
+                    json.dumps({'age': diagnosis_data.get('age')},
+                               ensure_ascii=False)),
+                'diagnosis': diagnosis(
+                    json.dumps(diagnosis_data.validated_data,
+                               ensure_ascii=False))
             }
+            [o.pop('_id') for o in data['similar_diagnosis']]
 
             return Response(data, status=status.HTTP_200_OK)
         else:
