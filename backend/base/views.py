24a90e8075dd2cf4bd612db328aede4981a87274
Andriy Taran <andreitdeveloper@gmail.com>
2018-02-01 09:07:45 +0100

    Add growth_by_specific_type to response from reports endpoint

--- backend/base/views.py
+++ backend/base/views.py
@@ -6,9 +6,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from base.serializers import DiagnosisSerializer
-from lib.dataset import breast_cancer_by_grade, diagnosis, surgery_decisions
+from lib.dataset import breast_cancer_by_grade, diagnosis, \
+    growth_by_specific_type
 from lib.dataset import woman_age_30_40_annualy_diagnosed, \
-    er_pos_pr_neg_her2_neg_annual_diagnoses, breast_cancer_by_size_age_30_40, \
+    breast_cancer_by_size_age_30_40, \
     distribution_of_stage_of_cancer_for_ages_30_40, \
     percent_of_women_with_cancer_by_race, surgery_decisions_within_ages_30_40, \
     chemotherapy_for_ages_30_40, radiation_for_ages_30_40, \
@@ -29,7 +30,9 @@ class ReportDataView(GenericAPIView):
 
             data = {
                 'woman_age_30_40_annualy_diagnosed': woman_age_30_40_annualy_diagnosed(),
-                'er_pos_pr_neg_her2_neg_annual_diagnoses': er_pos_pr_neg_her2_neg_annual_diagnoses(),
+                'growth_by_specific_type': growth_by_specific_type(
+                    '{"type": "Other", "type": "ILC", "type": "In Situ", "type": "IDC"}',
+                    "$or"),
                 'breast_cancer_by_grade_and_size_age_30_40': {
                     'grade': breast_cancer_by_grade(diagnosis_data.get('age')),
                     'size': breast_cancer_by_size_age_30_40()
@@ -44,9 +47,6 @@ class ReportDataView(GenericAPIView):
                     'cause_of_death_overall': cause_of_death_overall(),
                     'cause_of_death_within_ages_30_40': cause_of_death_within_ages_30_40()
                 },
-                'surgery_decisions': surgery_decisions(
-                    json.dumps({'age': diagnosis_data.get('age')},
-                               ensure_ascii=False)),
                 'diagnosis': diagnosis(
                     json.dumps(diagnosis_data.validated_data,
                                ensure_ascii=False))
