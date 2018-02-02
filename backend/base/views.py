10e1df3753c2aa2914917c772ca2be11a47efb17
Andriy Taran <andreitdeveloper@gmail.com>
2018-02-02 16:34:24 +0100

    Update Diagrams endpoint

--- backend/base/views.py
+++ backend/base/views.py
@@ -25,58 +25,56 @@ class ReportDataView(GenericAPIView):
 
         serializer = self.get_serializer(data=request.query_params)
 
-        if serializer.is_valid():
-            diagnosis_data = serializer.validated_data
+        serializer.is_valid(raise_exception=True)
+        diagnosis_data = serializer.validated_data
 
-            breakout_by_stage_d = breakout_by_stage(
-                json.dumps({"age": diagnosis_data.get('age'),
-                            "breast-adjusted-ajcc-6th-stage-1988": {
-                                "$in": ["I", "IIA", "IIB", "IIIA",
-                                        "IIIB", "IIIC", "IIINOS", "IV",
-                                        0]
-                            }}))
+        breakout_by_stage_d = breakout_by_stage(
+            json.dumps({"age": diagnosis_data.get('age'),
+                        "breast-adjusted-ajcc-6th-stage-1988": {
+                            "$in": ["I", "IIA", "IIB", "IIIA",
+                                    "IIIB", "IIIC", "IIINOS", "IV",
+                                    0]
+                        }}))
 
-            data = {
-                'woman_age_30_40_annualy_diagnosed': woman_age_30_40_annualy_diagnosed(),
-                'growth_by_specific_type': {
-                    'other': growth_by_specific_type(
-                        '{"type": "Other", "type": "Mixed", "type": "IBC"}',
-                        "$or"),
-                    'idc': growth_by_specific_type('{"type": "IDC"}', "$or"),
-                    'ilc': growth_by_specific_type('{"type": "ILC"}', "$or"),
-                    'in_situ': growth_by_specific_type('{"type": "In-Situ"}',
-                                                       "$or")
-                },
-                'breast_cancer_by_grade_and_size_age_30_40': {
-                    'grade': breast_cancer_by_grade(diagnosis_data.get('age')),
-                    'size': breast_cancer_by_size_age_30_40()
-                },
-                'distribution_of_stage_of_cancer_for_ages_30_40': distribution_of_stage_of_cancer_for_ages_30_40(),
-                'percent_of_women_with_cancer_by_race': {
-                    'overall': percent_of_women_with_cancer_by_race(),
-                    'by_age': percent_race_with_cancer_by_age(
-                        json.dumps(diagnosis_data,
-                                   ensure_ascii=False)),
-                },
-                'surgery_decisions_within_ages_30_40': surgery_decisions_within_ages_30_40(),
-                'chemotherapy_for_ages_30_40': {
-                    'overall': chemotherapy_for_ages_30_40(),
-                    'breakout_by_stage': breakout_by_stage_d
-                },
-                'radiation_for_ages_30_40': {
-                    'overall': radiation_for_ages_30_40(),
-                    'breakout_by_stage': breakout_by_stage_d
-                },
-                'survival_months_within_ages_30_40': survival_months_within_ages_30_40(),
-                'cause_of_death': {
-                    'cause_of_death_overall': cause_of_death_overall(),
-                    'cause_of_death_within_ages_30_40': cause_of_death_within_ages_30_40()
-                },
-                'similar_diagnosis': diagnosis(json.dumps(diagnosis_data,
-                                                          ensure_ascii=False))
-            }
-            [o.pop('_id') for o in data['similar_diagnosis']]
+        data = {
+            'woman_age_30_40_annualy_diagnosed': woman_age_30_40_annualy_diagnosed(),
+            'growth_by_specific_type': {
+                'other': growth_by_specific_type(
+                    '{"type": "Other", "type": "Mixed", "type": "IBC"}',
+                    "$or"),
+                'idc': growth_by_specific_type('{"type": "IDC"}', "$or"),
+                'ilc': growth_by_specific_type('{"type": "ILC"}', "$or"),
+                'in_situ': growth_by_specific_type('{"type": "In-Situ"}',
+                                                   "$or")
+            },
+            'breast_cancer_by_grade_and_size_age_30_40': {
+                'grade': breast_cancer_by_grade(diagnosis_data.get('age')),
+                'size': breast_cancer_by_size_age_30_40()
+            },
+            'distribution_of_stage_of_cancer_for_ages_30_40': distribution_of_stage_of_cancer_for_ages_30_40(),
+            'percent_of_women_with_cancer_by_race': {
+                'overall': percent_of_women_with_cancer_by_race(),
+                'by_age': percent_race_with_cancer_by_age(
+                    json.dumps(diagnosis_data,
+                               ensure_ascii=False)),
+            },
+            'surgery_decisions_within_ages_30_40': surgery_decisions_within_ages_30_40(),
+            'chemotherapy_for_ages_30_40': {
+                'overall': chemotherapy_for_ages_30_40(),
+                'breakout_by_stage': breakout_by_stage_d
+            },
+            'radiation_for_ages_30_40': {
+                'overall': radiation_for_ages_30_40(),
+                'breakout_by_stage': breakout_by_stage_d
+            },
+            'survival_months_within_ages_30_40': survival_months_within_ages_30_40(),
+            'cause_of_death': {
+                'cause_of_death_overall': cause_of_death_overall(),
+                'cause_of_death_within_ages_30_40': cause_of_death_within_ages_30_40()
+            },
+            'similar_diagnosis': diagnosis(json.dumps(diagnosis_data,
+                                                      ensure_ascii=False))
+        }
+        [o.pop('_id') for o in data['similar_diagnosis']]
 
-            return Response(data, status=status.HTTP_200_OK)
-        else:
-            return Response({}, status=status.HTTP_200_OK)
+        return Response(data, status=status.HTTP_200_OK)
