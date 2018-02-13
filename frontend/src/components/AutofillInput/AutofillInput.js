375f5cd5136a08522380094ab06c43b3f09a6112
Andriy Taran <andreitdeveloper@gmail.com>
2018-02-13 16:33:30 +0100

    Add missing file.

---
+++ frontend/src/components/AutofillInput/AutofillInput.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// TODO trying to fix autocomplete on iOS Chrome
+// https://github.com/facebook/react/issues/1159
+class AutofillInput extends React.Component {
+  componentDidMount(){
+    this.input.addEventListener('change', (e)=> {
+      console.log('change')
+      this.props.onChange(e)
+    })
+  }
+
+  refInput = (input) => this.input = input
+
+  render() {
+    const {...otherProps} = this.props
+
+    return (
+      <input
+        {...otherProps}
+        ref={this.refInput}
+      />
+    )
+  }
+}
+
+export default AutofillInput
