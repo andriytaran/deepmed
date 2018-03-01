defa5bd581595a7e87faa70dec7e7858607c180b
Andriy Taran <andreitdeveloper@gmail.com>
2018-03-01 11:43:39 +0100

    Update bars position in survival months chart.

--- frontend/src/routes/bcSurvivalMonths/BcSurvivalMonths.js
+++ frontend/src/routes/bcSurvivalMonths/BcSurvivalMonths.js
@@ -48,12 +48,12 @@ const chartsLabelsOptions = {
 }
 
 const white = '#fff'
-const color_1 = 'rgba(72, 204, 245, 0.5)'
+const color_1 = 'rgba(72, 204, 245, 0.75)'
 const color_2 = '#88d0d1'
 const color_3 = '#47cfd1'
 const color_4 = 'rgba(184, 232, 245, 0.5)'
 const color_5 = '#1ac6ff'
-const color_6 = '#8f61ec'
+const color_6 = 'rgba(143, 97, 236, 0.75)'
 const color_7 = '#9df51d'
 const color_8 = '#ff9400'
 const color_9 = '#f51431'
@@ -87,9 +87,9 @@ const SurvivalMonthsChart = ({data}) =>
       },
       scales: {
         xAxes: [{
-          stacked: true,
           id: 'bar-x-axis1',
-          barThickness: 50,
+          barPercentage: 1.0,
+          categoryPercentage: 0.3
         }],
         yAxes: [{
           ticks: {
