0441a6fcd82362171af86988c3e94772b52c0a99
Andriy Taran <andreitdeveloper@gmail.com>
2018-03-02 14:37:54 +0100

    Update css styles of GS page.

--- frontend/src/routes/bcGlobalStatistics/BcGlobalStatistics.js
+++ frontend/src/routes/bcGlobalStatistics/BcGlobalStatistics.js
@@ -2,25 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import s from './BcGlobalStatistics.css'
-import {Col} from '../../components'
 import map_worldwide from '../../static/global_statistics/map-worldwide.jpg'
 import bar_worldwide from '../../static/global_statistics/bar-worldwide.jpg'
-const chart_images = [
-  map_worldwide,
-  bar_worldwide
-]
+
 
 class BcGlobalStatistics extends React.Component {
   render() {
     return (
       <div className={s.container}>
-          <Col>
-            <div className={s.card}>
-              {chart_images.map((item, i) =>
-                <img className={s.image} src={item}/>
-              )}
-            </div>
-          </Col>
+        <div className={s.imageMapWrapper}>
+          <img className={s.imageMap} src={map_worldwide}/>
+        </div>
+        <div className={s.imageBarChartWrapper}>
+          <img className={s.imageBarChart} src={bar_worldwide}/>
+        </div>
       </div>
     )
   }
